8bebe9a0b340f4b8571084951b6d56bc4f13a9d7
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2021-07-11 00:45:10 +0530

    Edit JS FIle

--- todos_list.js
+++ todos_list.js
@@ -1,13 +1,25 @@
 
 //Initialize Storage
+// if(localStorage.length == null){
 var Todos = []
-
+// }
+// else {
+//     var Todos = localStorage.getItem("todos")
+//     console.log(typeof Todos)
+// }
 
 //Update Storage
-function update() {
-    localStorage.setItem("todos", Todos)
+var update = () =>  {
+    var data = localStorage.setItem("todos", Todos)
+    JSON.stringify(data)
 }
 
+// update()
+// function getData() {
+//     var data = localStorage.getItem("todos")
+//     console.log(JSON.parse(data));
+//     event.preventDefault()
+// }
 
 //Add Todos To Storage
 function addTodo(title,description) {
@@ -23,6 +35,7 @@ function addTodo(title,description) {
     } )
     console.log(Todos);
     update()
-
+    getData()
     event.preventDefault()
 }
+
