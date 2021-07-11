40f75722309a652b8055739d56496002d0f16aa7
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2021-07-11 17:23:05 +0530

    JS Edit

--- tableData.js
+++ tableData.js
@@ -1,6 +1,28 @@
-function tableShow() {
-    var data = JSON.stringify(localStorage.getItem("todo"))
-    console.log(data[0].title);
-    console.log(localStorage);
+
+if (localStorage.length == null) {
+    var Todo = [] 
 }
+else {
+    var json = localStorage.getItem('list')
+    Todo = JSON.parse(json)
+}
+var tableShow = () => {
+    console.log(localStorage)
+
+    if (localStorage.getItem('list') == null){
+        document.getElementById('table').innerText = "No Todos To Display"
+    }
+    else{
+    var details = localStorage.getItem('list');
+    console.log(details);
+    var array = JSON.parse(details)
+    console.log(array);
+    let tablebody = ""
+    for (let index = 0; index < array.length; index++) {
+        tablebody += "<tr><th>" + (index+1) + "</th><td>" + array[index].title + "</td><td>" + array[index].desc + "</td><td><button type='button'>Delete</button></td></tr>"
+        document.getElementById('tbody').innerHTML = tablebody
+    }
+}
+}
+
 tableShow()
